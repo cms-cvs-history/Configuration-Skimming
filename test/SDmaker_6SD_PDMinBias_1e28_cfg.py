015dd2499378ab7ac0f3c4f7e5de6e6756e97896
chiorbo <sha1-a37cc23738f7aa7c467ce16efb74e15501057467@cern.ch>
2010-04-30 14:28:57 +0000

    SD definition merged between the recent and the very old trigger tables for the reprocessing

--- test/SDmaker_6SD_PDMinBias_1e28_cfg.py
+++ test/SDmaker_6SD_PDMinBias_1e28_cfg.py
@@ -4,14 +4,14 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("makeSD")
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.6 $'),
+    version = cms.untracked.string('$Revision: 1.7 $'),
     annotation = cms.untracked.string('SD and central skims'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/Skimming/test/SDmaker_6SD_PDMinBias_1e28_cfg.py,v $')
 )
 
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(-1)
 )
 
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
@@ -23,32 +23,13 @@ process.GlobalTag.globaltag = "GR10_P_V4::All"
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/FC2EBD1F-134F-DF11-BE52-000423D94A20.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/FAFB20B8-0A4F-DF11-ABC9-000423D98F98.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/F23993AD-084F-DF11-AB0D-0030486780B4.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/E2E6DCFC-0E4F-DF11-8249-0030486730C6.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/D4041DD5-134F-DF11-AE0D-0030487CD718.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/D0B384AA-084F-DF11-AFEA-0030486780E6.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/CE88CA39-154F-DF11-BA79-000423D6006E.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/CA38846F-104F-DF11-9AD0-000423D944DC.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/BECAFB49-104F-DF11-A881-0030487CD7C6.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/BC1F1969-124F-DF11-B482-0030487C778E.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/BA7C9DDA-0C4F-DF11-9092-003048D2BE12.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/B8914532-0E4F-DF11-9980-0030487C6062.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/B8445CD5-054F-DF11-A76F-0030487C8CB6.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/B6019FBE-0A4F-DF11-9BC2-000423D985B0.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/A8D67778-0B4F-DF11-AFEB-000423D990CC.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/9C7112E9-074F-DF11-988B-001D09F251B8.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/70A67C35-074F-DF11-A631-001D09F295A1.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/5E2C413B-054F-DF11-8BAE-001D09F24498.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/58E767DA-0C4F-DF11-B33E-003048D2C0F4.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/56060E20-134F-DF11-9260-000423D9517C.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/4A29A188-144F-DF11-9BAC-0030487CD812.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/46795DB7-0A4F-DF11-8C02-000423D99264.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/4039E1D4-134F-DF11-8558-0030487CD7C6.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/2A7B1405-114F-DF11-83B4-0030487CD6F2.root',
-        '/store/data/Commissioning10/MinimumBias/RECO/v9/000/133/853/008A2EB9-034F-DF11-BB2A-000423D98920.root'
-
+        '/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/528/867BB35A-2B3E-DF11-AC1D-000423D99996.root',
+        '/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/526/5491FB8D-213E-DF11-B3A5-000423D6006E.root',
+        '/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/524/6A6E09B5-1C3E-DF11-BEA5-0030487CD17C.root',
+        '/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/523/0E6FF7ED-143E-DF11-98E6-000423D99BF2.root',
+        '/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/521/306927B4-023E-DF11-B2C3-000423D98BE8.root',
+        '/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/519/E431D7E8-F13D-DF11-AE5A-0030487CAF0E.root',
+        '/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/514/EC4F1421-E33D-DF11-B07D-0030487C6088.root'
         )
 )
 process.source.inputCommands = cms.untracked.vstring("keep *", "drop *_MEtoEDMConverter_*_*")
@@ -72,6 +53,8 @@ process.source.inputCommands = cms.untracked.vstring("keep *", "drop *_MEtoEDMCo
 #        'critical', 
 #        'cout')
 #)
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 import HLTrigger.HLTfilters.hltHighLevelDev_cfi
 
@@ -212,6 +195,7 @@ process.EG_1e28.HLTPaths = (
 "HLT_DoublePhoton4_Jpsi_L1R",
 "HLT_DoublePhoton4_Upsilon_L1R",
 "HLT_DoublePhoton4_eeRes_L1R",
+"HLT_DoublePhoton5_eeRes_L1R", #added to match the /cdaq/physics/firstCollisions10/v2.0/HLT_7TeV/V5 table
 "HLT_DoublePhoton5_Jpsi_L1R",
 "HLT_DoublePhoton5_Upsilon_L1R",
 "HLT_DoublePhoton5_L1R",
@@ -225,7 +209,7 @@ process.EG_1e28.HLTPaths = (
 "HLT_Ele10_LW_L1R",
 "HLT_Photon15_TrackIso_L1R"
 )
-process.EG_1e28.HLTPathsPrescales  = cms.vuint32(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
+process.EG_1e28.HLTPathsPrescales  = cms.vuint32(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
 process.EG_1e28.HLTOverallPrescale = cms.uint32(1)
 process.EG_1e28.throw = False
 process.EG_1e28.andOr = True
